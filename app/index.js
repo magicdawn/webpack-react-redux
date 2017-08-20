07e76e2a19cada6b1e531da5b0cfddc1051b3062
magicdawn <magicdawn@qq.com>
2017-08-20 17:40:16 +0800

    modify index

--- app/index.js
+++ app/index.js
@@ -4,26 +4,26 @@ import { AppContainer } from 'react-hot-loader'
 import { configureStore, history } from './store/configureStore'
 import Root from './containers/Root'
 
-const store = configureStore()
+const init = Root => {
+  render(
+    <AppContainer>
+      <Root store={store} history={history} />
+    </AppContainer>,
+    document.getElementById('root')
+  )
+}
 
-render(
-  <AppContainer>
-    <Root store={store} history={history} />
-  </AppContainer>,
-  document.getElementById('root')
-)
+const store = configureStore()
+init(Root)
 
 if (module.hot) {
   module.hot.accept('./containers/Root', () => {
-    const newConfigureStore = require('./store/configureStore')
-    const newStore = newConfigureStore.configureStore()
-    const newHistory = newConfigureStore.history
     const NewRoot = require('./containers/Root').default
-    render(
-      <AppContainer>
-        <NewRoot store={newStore} history={newHistory} />
-      </AppContainer>,
-      document.getElementById('root')
-    )
+    init(NewRoot)
+  })
+
+  module.hot.accept('./reducers', () => {
+    const reducer = require('./reducers/index').default
+    store.replaceReducer(reducer)
   })
 }
